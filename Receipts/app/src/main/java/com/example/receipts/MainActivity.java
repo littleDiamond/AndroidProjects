f66fd0be6614641a2a0c422bd2682d156b740fdf
littleDiamond <diamondzhou916@gmail.com>
2019-03-28 09:25:44 +1300

    Fixed the main activity

--- Receipts/app/src/main/java/com/example/receipts/MainActivity.java
+++ Receipts/app/src/main/java/com/example/receipts/MainActivity.java
@@ -21,18 +21,18 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the login button */
     public void logIn(View view) {
-//        Intent intent = new Intent(this, LoginActivity.class);
-//
-//        // send user name and password to login page
-//        EditText userNameText = findViewById(R.id.user_name);
-//        String userName = userNameText.getText().toString();
-//
-//        EditText passwordText = findViewById(R.id.password);
-//        String password = passwordText.getText().toString();
-//
-//        intent.putExtra(USER_NAME_MESSAGE, userName);
-//        intent.putExtra(PASSWORD_MESSAGE, password);
-//        startActivity(intent);
+        Intent intent = new Intent(this, LoginActivity.class);
+
+        // send user name and password to login page
+        EditText userNameText = findViewById(R.id.username);
+        String userName = userNameText.getText().toString();
+
+        EditText passwordText = findViewById(R.id.password);
+        String password = passwordText.getText().toString();
+
+        intent.putExtra(USER_NAME_MESSAGE, userName);
+        intent.putExtra(PASSWORD_MESSAGE, password);
+        startActivity(intent);
     }
 
 }
